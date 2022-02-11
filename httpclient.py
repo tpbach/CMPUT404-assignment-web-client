e783d2734f1f500417e00025b6e062f7ba36b81b
tpbach <tpbach@ualberta.ca>
2022-02-10 17:10:54 -0700

    cleanup and typo fix

--- httpclient.py
+++ httpclient.py
@@ -40,8 +40,7 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        data = data.split(" ")[1]
-        return int(data)
+        return int(data.split('\r\n')[0].split(" ")[1])
 
     def get_headers(self,data):
         return data.split("\r\n\r\n")[0]
@@ -66,7 +65,7 @@ class HTTPClient(object):
             else:
                 done = not part
         return buffer.decode('utf-8')
-
+    
     def GET(self, url, args=None):
         parsedURL = urllib.parse.urlparse(url)
         host = parsedURL.hostname
@@ -74,44 +73,29 @@ class HTTPClient(object):
         # Get port, otherwise set to default 80
         if parsedURL.port:
             port = parsedURL.port
-            portString = f":{port}"
         else:
             port = 80
-            portString = ""
 
         # Get path, otherwise set to default '/'
         if parsedURL.path:
             path = parsedURL.path
         else:
             path = "/"
-
         self.connect(host, port)
 
         getRequest= (
-            f"GET {path} HTTp/1.1\r\n"
-            f"Host: {host}{portString}\r\n"
-            f"User-Agent: httpclient/1.0\r\n"
+            f"GET {path} HTTP/1.1\r\n"
+            f"Host: {host}\r\n"
             f"Accept: */*\r\n"
-            f"Connection: close\r\n"
-            f"\r\n"
+            f"Connection: close\r\n\r\n"
         )
 
-        outputString = (
-            f"\n----------------- POST Request -----------------\n"
-            f"{getRequest}"
-            f"\n------------------------------------------------\n"
-        )
-        print(outputString)
+        self.print_output("GET", getRequest)
 
         self.sendall(getRequest)
-        response = self.recvall(self.socket)
+        response = str(self.recvall(self.socket))
 
-        outputString = (
-            f"\n----------------- Response -----------------\n"
-            f"{response}"
-            f"\n--------------------------------------------\n"
-        )
-        print(outputString)
+        self.print_output("", response)
 
         code = self.get_code(response)
         body = self.get_body(response)
@@ -125,11 +109,8 @@ class HTTPClient(object):
         # Get port, otherwise set to default 80
         if parsedURL.port:
             port = parsedURL.port
-            portString = f":{port}"
         else:
             port = 80
-            portString = ""
-
         self.connect(host, port)
 
         # Get path, otherwise set to default '/'
@@ -144,42 +125,41 @@ class HTTPClient(object):
         else:
             postBody = ""
 
-
         contentLength = len(postBody.encode())
 
         postRequest = (
             f"POST {path} HTTP/1.1\r\n"
-            f"Host: {host}{portString}\r\n"
-            f"User-Agent: httpclient/1.0\r\n"
+            f"Host: {host}\r\n"
             f"Content-Type: application/x-www-form-urlencoded\r\n"
             f"Content-Length: {contentLength}\r\n"
             f"Connection: close\r\n\r\n"
             f"{postBody}"
         )
         
-        outputString = (
-            f"\n----------------- POST Request -----------------\n"
-            f"{postRequest}"
-            f"\n------------------------------------------------\n"
-        )
-
-        print(outputString)
- 
-
+        self.print_output("POST", postRequest)
+        
         self.sendall(postRequest)
         response = self.recvall(self.socket)
 
-        outputString = (
-            f"\n----------------- Response -----------------\n"
-            f"{response}"
-            f"\n--------------------------------------------\n"
-        )
-        print(outputString)
+        self.print_output("", response)
 
         code = self.get_code(response)
         body = self.get_body(response)
         self.close()
         return HTTPResponse(code, body)
+
+    
+    def print_output(self, type, data):
+        if type == "GET":
+            header = "\n----------------- GET REQUEST -----------------\n"
+        elif type == "POST":
+            header = "\n----------------- POST REQUEST -----------------\n"
+        else:
+            header = "\n----------------- RESPONSE -----------------\n"
+
+        footer = "\n" + "-"*len(header) + "\n"
+        outputString = header + data + footer
+        print(outputString)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
